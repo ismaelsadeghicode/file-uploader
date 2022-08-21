76aad066bd6991ec20151fbedbf117b672f59440
e.sadeghi <e.sadeghi@fwutech.com>
2022-08-21 01:01:15 -0400

    Added react project

--- src/main/java/com/javatar/fileuploader/config/WebConfig.java
+++ src/main/java/com/javatar/fileuploader/config/WebConfig.java
@@ -3,7 +3,9 @@ package com.javatar.fileuploader.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.ResourceResolver;
 
 @Configuration
 @EnableWebMvc
@@ -13,4 +15,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        ResourceResolver resolver = new ReactResourceResolver();
+        registry.addResourceHandler("/**")
+                .resourceChain(true)
+                .addResolver(resolver);
+
+        // Can try to play with
+        // registry.addResourceHandler("/**")
+        //        .addResourceLocations("classpath:/static/");
+        // But this option can't map every path to index.html
+        // Can try https://stackoverflow.com/a/42998817/1032167
+        // to resolve this, but then you loose /api/** => rest
+        // and to be honest it is some regex madness, so
+        // it was easier for me to setup custom resource resolver
+    }
+
 }
--- src/main/java/com/javatar/fileuploader/controller/FileController.java
+++ src/main/java/com/javatar/fileuploader/controller/FileController.java
@@ -2,18 +2,11 @@ package com.javatar.fileuploader.controller;
 
 import com.javatar.fileuploader.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.tempuri.ResponseDTO;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class FileController {
 
@@ -40,37 +33,6 @@ public class FileController {
                 extorgName,
                 protocolName,
                 userName);
-    }
-
-//    @PostMapping(value = "/upload")
-//    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-//
-//        String fileName = fileService.storeFile(file);
-//
-//        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("/downloadFile/")
-//                .path(fileName)
-//                .toUriString();
-//
-//        return new UploadFileResponse(
-//                fileName,
-//                fileDownloadUri,
-//                file.getContentType(),
-//                file.getSize());
-//    }
-
-    @GetMapping("/downloadFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException {
-        Resource resource = fileService.loadFileAsResource(fileName);
-        String contentType = null;
-        contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        if (contentType == null) {
-            contentType = "application/octet-stream";
-        }
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename='\"" + resource.getFilename() + "'\"")
-                .body(resource);
     }
 
 }
